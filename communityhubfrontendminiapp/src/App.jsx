9e8ec73ce22567ee64f2f98cce743d5d3951224a
Prudez <prudencealice25@gmail.com>
2026-05-11 15:52:09 +0300

    communithubfrontendminiapp

---
+++ communityhubfrontendminiapp/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import PostDetail from "./pages/PostDetail";
+import CreatePost from "./pages/CreatePost";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="posts" element={<Posts />} />
+          <Route path="posts/:id" element={<PostDetail />} />
+          <Route path="create" element={<CreatePost />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
